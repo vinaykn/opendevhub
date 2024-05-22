38b6091b444f1e6589a36f4977882b293e20bbb7
vinaykn <vinaykn@users.noreply.github.com>
2024-05-22 18:32:43 +0530

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,9 @@
+export default {
+  // ...
+  url: 'https://endiliey.github.io', // Your website URL
+  baseUrl: '/',
+  projectName: 'endiliey.github.io',
+  organizationName: 'endiliey',
+  trailingSlash: false,
+  // ...
+};
